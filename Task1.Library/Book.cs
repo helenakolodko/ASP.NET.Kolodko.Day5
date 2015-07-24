32c83a57c36b63094f1f00993948d78d3eb7db49
helenakolodko <helena.kolodko@gmail.com>
2015-07-24 20:08:29 +0300

    Changed comparison of strings on equality on CurrentCultureIgnoreCase StringComparison.

--- Task1.Library/Book.cs
+++ Task1.Library/Book.cs
@@ -9,14 +9,41 @@ namespace Task1.Library
     [Serializable]
     public class Book : IEquatable<Book>, IComparable<Book>
     {
-        private string author;
         private string title;
         private int year;
         private int pages;
-        public string Author { get{return author;} set{author = value;} }
-        public string Title { get{return title;} set{if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(); else title = value;} }
-        public int Year { get{return year;} set{if (value < 0) throw new ArgumentOutOfRangeException(); else year = value;} }
-        public int Pages { get{return pages;} set{if (value < 1) throw new ArgumentOutOfRangeException(); else pages = value;} }
+        public string Author { get; set; }
+        public string Title
+        { 
+            get { return title; } 
+            set
+            {
+                if (string.IsNullOrEmpty(value)) 
+                    throw new ArgumentNullException(); 
+                else 
+                    title = value;
+            } 
+        }
+        public int Year 
+        { 
+            get { return year; } 
+            set
+            {
+                if (value < 0) 
+                    throw new ArgumentOutOfRangeException(); 
+                else year = value;
+            }
+        }
+        public int Pages 
+        { 
+            get { return pages; }
+            set 
+            {
+                if (value < 1) 
+                    throw new ArgumentOutOfRangeException(); 
+                else pages = value;
+            }
+        }
        
         public override int GetHashCode()
         {
@@ -34,7 +61,8 @@ namespace Task1.Library
                 return false;
             if (ReferenceEquals(other, this))
                 return true;
-            if (Author != other.Author || Title != other.Title || 
+            if (!String.Equals(Author, other.Author, StringComparison.CurrentCultureIgnoreCase) ||
+                !String.Equals(Title, other.Title, StringComparison.CurrentCultureIgnoreCase) || 
                 Year != other.Year || Pages != other.Pages)
                 return false;
             return true;
--- Task1.Library/BookListService.cs
+++ Task1.Library/BookListService.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Task1.Library
 {
@@ -76,7 +74,7 @@ namespace Task1.Library
 
         public List<Book> FindByTitle(string title)
         {
-            return FindAll((Book b) => b.Title == title);
+            return FindAll((Book b) => String.Equals(Title, other.Title, StringComparison.CurrentCultureIgnoreCase));
         }
 
         public void SortBookByTitle()
@@ -86,15 +84,14 @@ namespace Task1.Library
 
         public List<Book> FindByAuthor(string author)
         {
-            return FindAll((Book b) => b.Author == author);
+            return FindAll((Book b) => String.Equals(Title, other.Title, StringComparison.CurrentCultureIgnoreCase));
         }
 
         public void SortBookByAuthor()
         {
             Sort((Book b1, Book b2) => string.Compare(b1.Author, b2.Author, true));
         }
-
-
+        
         public List<Book> FindByYear(int year)
         {
             return FindAll((Book b) => b.Year == year);
